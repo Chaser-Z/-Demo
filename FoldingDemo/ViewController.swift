5f5aeb3b75ddf78fb1b5e34c166985cf5da35088
Chaser-Z <18330570306@163.com>
2016-11-03 16:13:03 +0800

    Initial Commit

---
+++ FoldingDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoldingDemo
+//
+//  Created by 张海南 on 2016/11/3.
+//  Copyright © 2016年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
