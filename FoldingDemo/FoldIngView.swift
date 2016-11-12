e41e40203c22ecacf4cefe8262e49274eaf63684
Chaser-Z <18330570306@163.com>
2016-11-12 15:27:09 +0800

    first commit

---
+++ FoldingDemo/FoldIngView.swift
@@ -0,0 +1,21 @@
+//
+//  FoldIngView.swift
+//  FoldingDemo
+//
+//  Created by 张海南 on 2016/11/3.
+//  Copyright © 2016年 枫韵海. All rights reserved.
+//
+
+import UIKit
+
+class FoldIngView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
